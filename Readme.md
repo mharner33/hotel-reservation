9b1e2b973bb98d2fffc97856dbf7b13bc82d59ef
Mike Harner <mharner33@gmail.com>
2023-06-08 10:30:37 -0600

    Added basic database connectivity - mongodb

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Requirements:
+- Fiber: go get github.com/gofiber/fiber/v2
+
+Mongodb
+- Docker: docker run mongodb -d mongo:latest -p 27017:27017
+
+Install Mongo driver:
+- go get go.mongodb.org/mongo-driver/mongo
--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/mharner33/hotel-reservation/handlers"
+	"github.com/mharner33/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gofiber/fiber/v2"
 )
+const dburi = "mongodb://192.168.86.30:27017"
+const dbname = "hotel-reservation"
+const userCollection = "users"
 
  func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userCollection)
+	// user := types.User{
+	// 	FirstName: "Mike",
+	// 	LastName: "Jones",
+	// }
+	// res, err := coll.InsertOne(ctx, user)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(res.InsertedID)
+
+	var mike types.User
+	err = coll.FindOne(ctx, bson.M{}).Decode(&mike)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mike)
 	listenPort := flag.String("listenPort", ":3000", "API server listen address")
 	flag.Parse()
 	// Fiber instance
